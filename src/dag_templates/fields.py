071b4ef3b5e7e3cb91025541efe758d0a6ba8081
Shivansh Saini <shivanshs9@gmail.com>
2020-08-15 14:13:23 +0530

    fix: fixed dag ID and cron expression validators
    - added numeric characters in dag ID regex
    - use croniter as fallback to validate cron expr if no preset match

--- src/dag_templates/fields.py
+++ src/dag_templates/fields.py
@@ -1,6 +1,8 @@
 import re
 
+from airflow.models.base import ID_LEN
 from airflow.utils.dates import cron_presets
+from croniter.croniter import CroniterError, croniter
 from wtforms import validators
 from wtforms.fields import *
 
@@ -8,27 +10,39 @@ from wtforms.fields import *
 class CronExpression(validators.Regexp):
     def __init__(self):
         regex = re.compile(
-            "^(?P<preset>{presets})$|^{0}\s+{1}\s+{2}\s+{3}\s+{4}$".format(
-                "(?P<minute>\*|[0-5]?\d)",
-                "(?P<hour>\*|[01]?\d|2[0-3])",
-                "(?P<day>\*|0?[1-9]|[12]\d|3[01])",
-                "(?P<month>\*|0?[1-9]|1[012])",
-                "(?P<day_of_week>\*|[0-6](\-[0-6])?)",
+            "^(?P<preset>{presets})$".format(
                 presets='|'.join(cron_presets.keys())
             )
         )
-        message = f'Invalid cron expression. Either use one of the presets ({", ".join(cron_presets.keys())}) or use crontab format (https://crontab.guru/)'
+        message = f'Invalid cron expression'
         super().__init__(regex, message=message)
+
+    def __call__(self, form, field, message=None):
+        try:
+            # try regex for cron presets
+            return super().__call__(form, field, message=message)
+        except validators.ValidationError as e:
+            # check validation using croniter
+            try:
+                croniter.expand(field.data or '')
+            except CroniterError as e:
+                msg = message or self.message
+                raise validators.ValidationError(f'{msg}: {e}')
+            else:
+                return True
 
 
 field_dag_id = StringField(
     'Dag ID', validators=(
-        validators.optional(), validators.regexp("^[a-zA-Z._-]+$")),
+        validators.optional(), validators.regexp("^[a-zA-Z0-9._-]+$"), validators.length(max=ID_LEN)),
     description='Dag ID is made of alphanumeric characters, dashes, dots and underscores exclusively.'
 )
 
 field_schedule_interval = StringField(
-    'Cron expression', validators=(validators.required(), CronExpression()))
+    'Cron expression', validators=(
+        validators.required(), CronExpression()),
+    description=f'Specify the schedule interval in standard crontab format (https://crontab.guru/) or use one of the presets ({", ".join(cron_presets.keys())})'
+)
 
 field_category = StringField(
     'Category', default='default', validators=(validators.optional(), validators.length(max=50)))
