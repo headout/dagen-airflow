931cab900ae71484c93c9dada5825c348c76ba97
Shivansh Saini <shivanshs9@gmail.com>
2020-10-11 15:40:45 +0530

    fix(ui): version change bug for preset select

--- src/www/static/dagen/js/edit.js
+++ src/www/static/dagen/js/edit.js
@@ -5,9 +5,13 @@ function syncFieldsWithVersion(val) {
     return;
   }
   for (const [field, val] of Object.entries(version)) {
-    query = $(`.conf-field#${field}`);
+    query = $(`.conf-field[name="${field}"]`);
     if ("boolean" === typeof val) query[0].checked = val;
-    else query.val(val);
+    else {
+      query.val(val);
+      // manually trigger the 'change' event of select input
+      if (query.is("select")) query.trigger("change");
+    }
   }
 }
 
