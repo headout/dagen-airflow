9a5c9495a6240cfa5ff0595c0e207c566fbbcc1e
Joel K Biju <joel.biju@headout.com>
2026-01-03 19:11:18 +0530

    simplify DagenDag live_version relationship - remove foreign_keys parameter

--- src/models.py
+++ src/models.py
@@ -41,7 +41,6 @@ class DagenDag(Base):
         'DagenDagVersion',
         primaryjoin='and_(DagenDagVersion.dag_id == DagenDag.dag_id, '
                     'DagenDagVersion.version == DagenDag._live_version)',
-        foreign_keys='[DagenDagVersion.dag_id, DagenDagVersion.version]',
         lazy='immediate',
         uselist=False,
         viewonly=True
